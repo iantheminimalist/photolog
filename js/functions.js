29c2c299290d7f7dcb7a569b8c7272e9d3446ee9
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2023-08-20 21:13:05 -0700

    added disablility function to image selector.

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,48 @@
+const imageSelected = (btn) =>{
+    var targetImg = document.getElementById(btn);
+    classListArr = targetImg.previousElementSibling.classList.contains("clicked");
+    let mainContainer = document.getElementsByClassName("main-container");
+    mainContainer[0].classList.add("disabled");
+    console.log(classListArr);
+    if(!classListArr){
+        targetImg.previousElementSibling.classList.add("clicked")
+        targetImg.parentElement.parentElement.classList.add("clicked")
+
+        const updatedWidth = window.innerWidth;
+        const updatedHeight = window.innerHeight;
+
+        console.log(`Updated browser width: ${updatedWidth}px`);
+        console.log(`Updated browser height: ${updatedHeight}px`);
+
+        targetImg.previousSibling.style = `
+            width: ${(updatedWidth - 400)}px; 
+            height: ${updatedHeight - 200}px; top: -100px;`;  
+
+        console.log(targetImg.parentElement.parentElement);
+    }else if(classListArr){
+        targetImg.previousElementSibling.classList.remove("clicked")
+        targetImg.parentElement.parentElement.classList.remove("clicked")
+        targetImg.previousSibling.style.width= null;
+        targetImg.previousSibling.style.height= null;
+        targetImg.previousSibling.style.top= 0;
+        mainContainer[0].classList.remove("disabled");
+    }
+
+    function updateSelectedImg(){
+
+        var allImgClicked = document.querySelectorAll(".imgItem-container .clicked");
+        allImgClicked.forEach(function(element) {
+            const newWidth = window.innerWidth * 0.5; // Set a new width based on screen width
+            const newHeight = window.innerHeight * 0.5; // Set a new height based on screen height
+            element.style.width = `${newWidth}px`;
+            element.style.height = `${newHeight}px`;
+        })
+
+    }
+    
+    window.addEventListener('resize', updateSelectedImg);
+}
+
 const imgGrabber = async (item) =>{
     const response = await fetch(`https://picsum.photos/1920/1080`);
     const imgContainer = document.getElementById("imgLog");
@@ -43,46 +88,21 @@ const imgGrabber = async (item) =>{
     imgContainer.appendChild(divElement);
     
     btnElement.addEventListener("click", (e) => {
-        var targetImg = document.getElementById(e.target.id);
-        classListArr = targetImg.previousElementSibling.classList.contains("clicked");
-        console.log(classListArr);
-        if(!classListArr){
-            targetImg.previousElementSibling.classList.add("clicked")
-            targetImg.parentElement.parentElement.classList.add("clicked")
-    
-            const updatedWidth = window.innerWidth;
-            const updatedHeight = window.innerHeight;
-    
-            console.log(`Updated browser width: ${updatedWidth}px`);
-            console.log(`Updated browser height: ${updatedHeight}px`);
-    
-            targetImg.previousSibling.style = `
-                width: ${(updatedWidth - 400)}px; 
-                height: ${updatedHeight - 200}px; top: -100px;`;  
-    
-            console.log(targetImg.parentElement.parentElement);
-        }else if(classListArr){
-            targetImg.previousElementSibling.classList.remove("clicked")
-            targetImg.parentElement.parentElement.classList.remove("clicked")
-            targetImg.previousSibling.style.width= null;
-            targetImg.previousSibling.style.height= null;
-            targetImg.previousSibling.style.top= 0;
-        }
-        
+
+        imageSelected(e.target.id)
     })
 
 }
 
-
-    const fetchImages = async () => {
-        const promises = [];
-        for (let i = 0; i < 4; i++) {
-          promises.push(imgGrabber(i));
-        }
-        await Promise.all(promises);
-      };
-      
-      fetchImages();
+const fetchImages = async () => {
+    const promises = [];
+    for (let i = 0; i < 8; i++) {
+        promises.push(imgGrabber(i));
+    }
+    await Promise.all(promises);
+    };
+    
+    fetchImages();
 
 
 
